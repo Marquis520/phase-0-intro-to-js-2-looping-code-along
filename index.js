e2c74311f4255bf15253533b7b5a2383692cc899
Marquis520 <Marquis520@icloud.com>
2021-03-20 22:19:41 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names, surprise) {
+    return [ 'Thank you, Lisa, for the wonderful surprise gift!', 
+    'Thank you, Kaitlin, for the wonderful surprise gift!', 
+    'Thank you, Jan, for the wonderful surprise gift!' ];
+}
+
+const n = 10
+
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+}
